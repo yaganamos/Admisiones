99437816527541862f2972a594ffd31c28e03c5d
carlos <yaganamosmil@gmail.com>
2015-06-09 16:54:11 -0700

    added name

--- app/admin/admission_request.rb
+++ app/admin/admission_request.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register AdmissionRequest do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :status
+permit_params :status, :name
 #
 # or
 #
