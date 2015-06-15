5b1759008330f582d8eeff45d41a96635c04d215
carlos <yaganamosmil@gmail.com>
2015-06-15 16:46:50 -0700

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
     resources :appointments
   end
 
-
+  break if ARGV.join.include? 'assets:precompile'
   root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
