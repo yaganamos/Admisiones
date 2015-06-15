2402e4b130a973d524501c169a7943e9c39850b1
carlos <yaganamosmil@gmail.com>
2015-06-15 16:31:37 -0700

    cambios cm

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module Projecto
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.precompile += %w( active_admin.css active_admin/print.css active_admin.js )
+    config.assets.initialize_on_precompile = false
   end
 end
